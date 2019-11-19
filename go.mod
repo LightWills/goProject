739059d3914e24c8d26cf8b35ceaad8e948f4d85
Williams <williamsviyou97@gmail.com>
2019-11-19 10:49:07 +0100

    With redi with goland

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gotest/awesomeProject
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3 // indirect
+require (
+	github.com/go-redis/redis v6.15.6+incompatible // indirect
+	github.com/go-redis/redis/v7 v7.0.0-beta.4 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 )
 
+var client *redis.Client
 var templates *template.Template
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
 	templates = template.Must(templates.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler).Methods("GET")
@@ -17,5 +23,10 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+	val, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		panic(err)
+	}
+	templates.ExecuteTemplate(w, "index.html", val)
+
 }
