9d3e15f5e26bee59e4c89389920073af625fae47
Williams <williamsviyou97@gmail.com>
2019-11-19 11:21:34 +0100

    Post Request

--- main.go
+++ main.go
@@ -17,16 +17,25 @@ func main() {
 
 	templates = template.Must(templates.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 	val, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
 		panic(err)
 	}
 	templates.ExecuteTemplate(w, "index.html", val)
+
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment_element := r.PostForm.Get("comment_element")
+	client.LPush("comments", comment_element)
+	http.Redirect(w, r, "/", 302)
 
 }
