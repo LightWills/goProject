2eb9013c47dcc1c2c4d2a6f3e9f24a6aeea0ca28
Williams <williamsviyou97@gmail.com>
2019-11-19 02:50:50 +0100

    change route

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloWord).Methods("GET")
+	r.HandleFunc("/bye", goodbye).Methods("GET")
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler (w http.ResponseWriter ,r *http.Request){
-	fmt.Fprint(w, "Hello my server run")
+func helloWord(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Choupina")
+}
+
+func goodbye(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "By Choupina ")
 }
