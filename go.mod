2e8e30c05236c6e6238a373c88aaf3b1c0aa0f65
Williams <williamsviyou97@gmail.com>
2019-11-19 14:20:33 +0100

    Session

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module gotest/awesomeProject
 go 1.13
 
 require (
-	github.com/go-redis/redis v6.15.6+incompatible // indirect
-	github.com/go-redis/redis/v7 v7.0.0-beta.4 // indirect
-	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/go-redis/redis v6.15.6+incompatible
+	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/sessions v1.2.0
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"html/template"
 	"net/http"
 )
 
 var client *redis.Client
 var templates *template.Template
+var store = sessions.NewCookieStore([]byte("kjsdjc7675yjhbks"))
 
 func main() {
 	client = redis.NewClient(&redis.Options{
@@ -19,6 +21,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexGetHandler).Methods("GET")
 	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	r.HandleFunc("/login", loginGetHandler).Methods("GET")
+	r.HandleFunc("/login", loginPostHandler).Methods("POST")
+	r.HandleFunc("/test", testGetHandler).Methods("GET")
+
 	fs := http.FileServer(http.Dir("./static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
@@ -40,4 +46,34 @@ func indexPostHandler(w http.ResponseWriter, r *http.Request) {
 	client.LPush("comments", comment_element)
 	http.Redirect(w, r, "/", 302)
 
+}
+
+func loginGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", nil)
+
+}
+
+func loginPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("name")
+	session, _ := store.Get(r, "session")
+	session.Values["username"] = username
+	session.Save(r, w)
+
+	templates.ExecuteTemplate(w, "login.html", nil)
+
+}
+
+func testGetHandler(w http.ResponseWriter, r *http.Request) {
+
+	session, _ := store.Get(r, "session")
+	untyped, ok := session.Values["username"]
+	if !ok {
+		return
+	}
+	username, ok := untyped.(string)
+	if !ok {
+		return
+	}
+	w.Write([]byte(username))
 }
